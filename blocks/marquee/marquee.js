12b934b900e0870487aef61d5650b58852e815f7
Gaur <2480603@cognizant.com>
2026-03-12 16:17:21 +0530

    adding blocks

---
+++ blocks/marquee/marquee.js
@@ -0,0 +1,45 @@
+
+//  export default function decorate(block) {
+
+//   const rows = [...block.children];
+
+//   rows.forEach((row) => {
+
+//     const wrapper = row.firstElementChild;
+//     if (!wrapper) return;
+
+//     const items = [...wrapper.children];
+
+//     const icon = items[0];
+//     const number = items[1];
+//     const label = items[2];
+
+//     const statsItem = document.createElement('div');
+//     statsItem.className = 'carousel-item';
+
+//     const contentDiv = document.createElement('div');
+//     contentDiv.className = 'carousel-text';
+
+//     const iconDiv = document.createElement('div');
+//     iconDiv.className = 'carousel-body';
+
+//     const numberDiv = document.createElement('div');
+//     numberDiv.className = 'carousel-title';
+
+//     const labelSpan = document.createElement('span');
+//     labelSpan.className = 'carousel-text';
+
+//     if (icon) iconDiv.append(icon);
+//     if (number) numberDiv.append(number.textContent);
+//     if (label) labelSpan.append(label.textContent);
+
+//     contentDiv.append(numberDiv, labelSpan);
+
+//     statsItem.append(iconDiv, contentDiv);
+
+//     row.replaceChildren(statsItem);
+
+//   });
+
+// }
+
